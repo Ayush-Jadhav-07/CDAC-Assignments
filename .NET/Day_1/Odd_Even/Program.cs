c0766ca4cfaa2a0c5d0b10782971fee17efc3016
Ayush Ankush Jadhav <aayushjadhav32@gmail.com>
2025-12-17 14:23:36 +0530

    Added .Net Day 1 Code

---
+++ .NET/Day_1/Odd_Even/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Odd_Even
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (checkNum(num))
+            {
+                Console.WriteLine($"{num} is Even Number");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is Odd Number");
+            }
+        }
+
+        private static bool checkNum(int num)
+        {
+            if (num == 0)
+            {
+                return true;
+            }
+            if (num % 2 == 0) return true;
+
+            return false;
+        }
+    }
+}
