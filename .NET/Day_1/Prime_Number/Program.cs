787ee05b1c81ee396c1a73fb36d7c67213e660b0
Ayush Ankush Jadhav <aayushjadhav32@gmail.com>
2025-12-17 14:24:39 +0530

    Added Day 1 .Net Program of Prime numbers

---
+++ .NET/Day_1/Prime_Number/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Prime_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (isPrime(num))
+            {
+                Console.WriteLine($"{num} is a prime number"); 
+            }
+            else
+            {
+                Console.WriteLine($"{num} is not a prime number");
+            }
+        }
+
+        private static bool isPrime(int num)
+        {
+            if (num <= 1)
+            {
+                return false;
+            }
+            if (num == 2) return true;
+
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
