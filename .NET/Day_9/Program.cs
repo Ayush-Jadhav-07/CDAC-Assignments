3287b1c84e7fb1c58e759bd168dd1e66126234d4
Ayush Ankush Jadhav <aayushjadhav32@gmail.com>
2025-12-29 19:10:00 +0530

    Add files via upload

---
+++ .NET/Day_9/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=IETDB2;Integrated Security=True";
+
+
+        static void Main(string[] args)
+        {
+           
+
+            Console.Write("Enter Username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Enter Password: ");
+            string password = Console.ReadLine();
+
+            bool isValid = CheckCredentials(username, password);
+
+            if (isValid)
+            {
+                Console.WriteLine("\nResult: Valid - Login Successful!");
+            }
+            else
+            {
+                Console.WriteLine("\nResult: Invalid - Wrong Username or Password");
+            }
+
+           
+        }
+
+        static bool CheckCredentials(string username, string password)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "SELECT COUNT(*) FROM emp WHERE username = @username AND password = @password";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@password", password);
+
+                    int count = (int)cmd.ExecuteScalar();
+                    return count > 0;
+                }
+
+            }
+        }
+
+
+
+
+    }
+}
+
